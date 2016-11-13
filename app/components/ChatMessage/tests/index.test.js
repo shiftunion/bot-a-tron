4c450fae6c7f820af84c38af84350c40f563527c
ShiftUnion <nick.cahill@gmail.com>
2016-11-14 10:00:13 +1300

    Bot pages

--- app/components/ChatMessage/tests/index.test.js
+++ app/components/ChatMessage/tests/index.test.js
@@ -2,7 +2,6 @@ import expect from 'expect';
 import { shallow } from 'enzyme';
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-
 import messages from '../messages';
 import Footer from '../index';
 import A from 'components/A';
